e31220b3c53da517b008f8694c6164e084fbea3b
Danil Anisimov <anisimov_d@joom.com>
2024-03-17 16:49:56 +0400

    more logs

--- tg/service.go
+++ tg/service.go
@@ -57,7 +57,7 @@ type handler struct {
 }
 
 func (s *Service) RegisterHandler(endpoint string, name string, f tele.HandlerFunc) {
-	slog.Info("Registered handler", slog.String("endpoint", endpoint), slog.String("handler", name))
+	slog.Info("registered handler", slog.String("endpoint", endpoint), slog.String("name", name))
 	s.handlers[endpoint] = append(s.handlers[endpoint], handler{
 		name: name,
 		f:    f,
