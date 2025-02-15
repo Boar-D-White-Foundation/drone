9cdaa41bf1a494810ee24411878f6258335f4ba2
Danil Anisimov <anisimov_d@joom.com>
2025-02-15 02:13:31 +0000

    added okr template test

---
+++ boardwhite/okr_test.go
@@ -0,0 +1,21 @@
+package boardwhite
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestBuildOkrMsg(t *testing.T) {
+	t.Parallel()
+
+	counts := make(map[okrTag]int)
+	i := 0
+	for tag := range okrGoals {
+		counts[tag] = i
+		i++
+	}
+	msg, err := buildOkrProgressMsg(counts)
+	require.NoError(t, err)
+	require.NotEmpty(t, msg)
+}
